1bd68038621bcaa4a9aedee8d361150da45d717c
João Victor Pessoa <joao.pessoa@uber.com>
2025-01-17 05:13:59 -0300

    Use Bloc

--- lib/features/items/ItemsScreen.dart
+++ lib/features/items/ItemsScreen.dart
@@ -1,42 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:roulette/features/items/ItemsViewModel.dart';
 
 import 'ItemsUIState.dart';
 
-class ItemsScreen extends StatefulWidget {
-
+class ItemsScreen extends StatelessWidget {
   static const String routeName = '/items';
 
   static Uri uri() {
     return Uri(path: routeName);
   }
 
-  static ItemsScreen goRouterBuilder(
-      BuildContext context,
-      GoRouterState state
-  ) {
-    return ItemsScreen(
-      viewModel: ItemsViewModel(),
+  static Widget goRouterBuilder(BuildContext context, GoRouterState state) {
+    return BlocProvider(
+      create: (_) => ItemsBloc(),
+      child: ItemsScreen(),
     );
   }
 
-  const ItemsScreen({
-    super.key,
-    required this.viewModel
-  });
-
-  final ItemsViewModel viewModel;
-
-  @override
-  State<ItemsScreen> createState() => _ItemsScreenState();
-}
-
-class _ItemsScreenState extends State<ItemsScreen> {
-  final scrollController = ScrollController();
-
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -44,127 +29,74 @@ class _ItemsScreenState extends State<ItemsScreen> {
           "Add Items",
         ),
       ),
-      body: ListenableBuilder(
-        listenable: widget.viewModel,
-        builder: (context, _) => Center(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent
-            child: ListView.builder(
-                itemCount: widget.viewModel.value.items.length,
-                controller: scrollController,
-                reverse: false,
-                itemBuilder: (context, index) {
-                  Item item = widget.viewModel.value.items[index];
-
-                  return Dismissible(
-                    key: Key(item.id),
-                    direction: DismissDirection.endToStart,
-                    onDismissed: (_) {
-                      widget.viewModel.removeItem(index);
-                    },
-                    background: Container(
-                      color: Colors.red,
-                      child: const Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Padding(
-                            padding: EdgeInsets.only(right: 16),
-                            child: Icon(Icons.delete),
-                          ),
-                        ],
-                      ),
-                    ),
-                    child: Column(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.only(
-                              left: 16,
-                              right: 16,
-                              top: 8,
-                              bottom: 8
-                          ),
-                          child: BaseTextField(
-                            key: Key(item.id),
-                            initialValue: item.text,
-                            onChanged: (value) => widget.viewModel.updateItem(index, value),
-                            focused: index == widget.viewModel.value.focusedItem,
-                            onFocused: () => widget.viewModel.onItemFocus(),
-                          ),
-                        ),
-                        Divider(
-                          height: 1,
-                        )
-                      ],
-                    ),
-                  );
-                }
-            )
-        ),
+      body: BlocBuilder<ItemsBloc, ItemsUIState>(
+        buildWhen: (previous, current) {
+          return previous.items != current.items;
+        },
+        builder: (context, state) {
+          return Center(
+            // Center is a layout widget. It takes a single child and positions it
+            // in the middle of the parent
+              child: Form(
+                child: ListView(
+                    reverse: false,
+                    children: [
+                      for (var index = 0; index < state.items.length; index++)
+                        buildItemTile(context, state.items[index], index)
+                    ]),
+              ));
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          widget.viewModel.addItem();
+          context.read<ItemsBloc>().add(AddItemPressed());
         },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
-}
 
-class BaseTextField extends StatefulWidget {
-  const BaseTextField({
-    super.key,
-    required this.initialValue,
-    required this.onChanged,
-    required this.focused,
-    required this.onFocused,
-  });
-
-  final bool focused;
-
-  final String initialValue;
-
-  final ValueChanged<String> onChanged;
-
-  final void Function() onFocused;
-
-  @override
-  State<BaseTextField> createState() => _BaseTextFieldState();
-}
-
-class _BaseTextFieldState extends State<BaseTextField> with SingleTickerProviderStateMixin, AutomaticKeepAliveClientMixin {
-  late AnimationController _controller;
-  var hasFocused = false;
-
-  final focusNode = FocusNode();
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(vsync: this);
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    super.build(context);
-
-    return TextFormField(
-      initialValue: widget.initialValue,
-      onChanged: widget.onChanged,
-      decoration: InputDecoration(
-        border: InputBorder.none,
+  Widget buildItemTile(BuildContext context, Item item, int index) {
+    return Dismissible(
+      key: Key(item.id),
+      direction: DismissDirection.endToStart,
+      onDismissed: (_) {
+        context.read<ItemsBloc>().add(DismissedItem(pos: index));
+      },
+      background: Container(
+        color: Colors.red,
+        child: const Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(right: 16),
+              child: Icon(Icons.delete),
+            ),
+          ],
+        ),
       ),
-      focusNode: focusNode,
+      child: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(
+                left: 16, right: 16, top: 8, bottom: 8),
+            child: TextFormField(
+                initialValue: item.text,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                ),
+                onChanged: (text) {
+                  context
+                      .read<ItemsBloc>()
+                      .add(ItemTextChanged(pos: index, text: text));
+                }),
+          ),
+          Divider(
+            height: 1,
+          )
+        ],
+      ),
     );
   }
-
-  @override
-  bool get wantKeepAlive => true;
 }
--- lib/features/items/ItemsViewModel.dart
+++ lib/features/items/ItemsViewModel.dart
@@ -1,33 +1,53 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'ItemsUIState.dart';
 
-class ItemsViewModel extends ValueNotifier<ItemsUIState> {
-  ItemsViewModel() : super(ItemsUIState(items: []));
+sealed class ItemsEvent {}
+final class AddItemPressed extends ItemsEvent {}
+final class DismissedItem extends ItemsEvent {
+  final int pos;
+  DismissedItem({required this.pos});
+}
+final class ItemTextChanged extends ItemsEvent {
+  final int pos;
+  final String text;
+  ItemTextChanged({required this.pos, required this.text});
+}
 
-  void addItem() {
-    value = value.copyWith(
-      items: value.items..add(Item(text: "")),
-      focusedItem: value.items.length - 1
+class ItemsBloc extends Bloc<ItemsEvent, ItemsUIState> {
+  ItemsBloc() : super(ItemsUIState(items: [
+    for (var i = 0; i < 10000; i++) Item(text: "Item $i")
+  ])) {
+    on<AddItemPressed>(_onAddItemPressed);
+    on<DismissedItem>(_onDismissedItem);
+    on<ItemTextChanged>(_onItemTextChange);
+  }
+
+  void _onAddItemPressed(event, emit) {
+    emit(
+        state.copyWith(
+            items: state.items..add(Item(text: "")),
+            focusedItem: state.items.length - 1
+        )
     );
   }
 
-  void removeItem(pos) {
-    value = value.copyWith(
-        items: value.items..removeAt(pos),
+  void _onDismissedItem(DismissedItem event, emit) {
+    emit(
+        state.copyWith(
+            items: state.items..removeAt(event.pos),
+        )
     );
   }
 
-  void updateItem(int pos, String text) {
-    var updatedItem = value.items[pos].copyWith(text: text);
-    value = value.copyWith(
-        items: value.items..[pos] = updatedItem,
-    );
-  }
-
-  void onItemFocus() {
-    value = value.copyWith(
-        focusedItem: null
+  void _onItemTextChange(ItemTextChanged event, emit) {
+    emit(
+        state.copyWith(
+            items: state.items..[event.pos] =
+            state.items[event.pos]
+                .copyWith(text: event.text),
+        )
     );
   }
 }
