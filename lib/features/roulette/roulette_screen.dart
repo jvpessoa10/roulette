3a9f460059efc3b9d6cabbc395a5a12973b56322
João Victor Pessoa <joao.pessoa@uber.com>
2025-01-25 19:45:06 -0300

    Add basic animation

--- lib/features/roulette/roulette_screen.dart
+++ lib/features/roulette/roulette_screen.dart
@@ -1,8 +1,12 @@
 
 
+import 'dart:async';
+import 'dart:ui';
+
 import 'package:flame/components.dart';
-import 'package:flame/events.dart';
+import 'package:flame/effects.dart';
 import 'package:flame/game.dart';
+import 'package:flame/geometry.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -49,36 +53,79 @@ class RouletteCenter extends ShapeComponent {
       anchor: Anchor.center
   );
 
+  final pi = 3.141592653589793;
+
+  var speed = 100.0;
+
+  var elapsedTime = 0.0;
+
+  final numItems = 20;
+  
+  final colors = [
+    Colors.red,
+    Colors.green,
+    Colors.blue,
+    Colors.yellow,
+    Colors.purple,
+    Colors.orange,
+    Colors.pink,
+    Colors.teal,
+  ];
+
+  @override
+  FutureOr<void> onLoad() {
+    final spinEffect = RotateEffect.by(
+        tau * 100,
+        EffectController(
+            duration: 10,
+            curve: Curves.easeOutQuint,
+        )
+    );
+    add(spinEffect);
+  }
+
   @override
   void render(Canvas canvas) {
+    super.render(canvas);
     if (renderShape) {
-      final pi = 3.14159265359;
-
       final rect = Rect.fromCenter(center: Offset.zero, width: 400, height: 400);
 
-      final numItems = 10;
-
       for (var i =0; i < numItems; i++) {
-        final startAngle = (2*pi / numItems) * (i + 1);
-        final sweepAngle = 2*pi / numItems;
+        final startAngle = (tau / numItems) * (i + 1);
+        final sweepAngle = tau / numItems;
 
+        final color = colors[i % colors.length];
 
         final itemPaint = Paint()
-          ..color = Color.fromRGBO(255-i*20 , i*20, i*30, opacity)
-          ..filterQuality = FilterQuality.low;
+          // ..shader = Gradient.linear(
+          //   Offset(0, 0),
+          //   Offset(1, 1),
+          //   [
+          //     color,
+          //     color.withOpacity(0.5),
+          //   ],
+          // )
+          ..color = colors[i % colors.length];
+
+
 
         canvas.drawArc(rect, startAngle, sweepAngle, true, itemPaint);
       }
 
-      // final startAngle = (2*pi / 10) * 3;
-      // final sweepAngle = 2*pi / 10;
-      //
-      // final whitePaint = Paint()
-      //   ..color = Colors.white;
-      //
-      // final startAngle2 = 2*pi / 10;
-      // final sweepAngle2 = 2*pi / 10;
-      // canvas.drawArc(rect, startAngle2, sweepAngle2, true, whitePaint);
+      final outerRect = Rect.fromCenter(center: Offset.zero, width: 410, height: 410);
+      final border = Paint()
+        ..color = Color(0xFFFFECB4)
+        ..style = PaintingStyle.stroke
+        ..strokeWidth = 10;
+
+      final border2 = Paint()
+        ..color = Color(0xFF8A0000)
+        ..style = PaintingStyle.stroke
+        ..strokeWidth = 5;
+
+      canvas.drawArc(outerRect, 0, tau, false, border);
+      canvas.drawArc(outerRect, 0, tau, false, border2);
+
     }
   }
 }
